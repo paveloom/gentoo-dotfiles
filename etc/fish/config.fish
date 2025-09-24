f82e8e4d4e2f8792a2cc81847e26a55aaf50fc87
Pavel Sobolev <contact@paveloom.dev>
2025-09-24 13:50:15 +0300

    Fish: disable the greeting.

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -3,6 +3,9 @@
 # Files in conf.d can be overridden by the user
 # by files with the same name in $XDG_CONFIG_HOME/fish/conf.d
 
+# Disable the greeting
+set -U fish_greeting
+
 if status is-interactive
     # Allow jumping back to Bash if necessary
     alias bash='env NOFISH=1 bash'
