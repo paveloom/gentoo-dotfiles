7cade5204bd57c684be1e8cbdba6044543edac6d
Pavel Sobolev <contact@paveloom.dev>
2025-09-27 11:13:41 +0300

    Instruct both Bash and Fish how to connect to the SSH agent [1].
    [1] https://www.gnupg.org/(fr)/documentation/manuals/gnupg/Agent-Examples.html

--- home/paveloom/.config/fish/config.fish
+++ home/paveloom/.config/fish/config.fish
@@ -4,6 +4,7 @@ if status is-interactive
     set __fish_git_prompt_char_stateseparator ""
 
     # Use the `gpg-agent` for the SSH protocol
+    set -e SSH_AGENT_PID
     set -q gnupg_SSH_AUTH_SOCK_by; or set -l gnupg_SSH_AUTH_SOCK_by 0
     if test $gnupg_SSH_AUTH_SOCK_by -ne %self
         gpgconf --launch gpg-agent
