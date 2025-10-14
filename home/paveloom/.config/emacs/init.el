9d30e92254a47a0e80ac67ba369b754cb47e44d8
Pavel Sobolev <contact@paveloom.dev>
2025-10-14 16:19:31 +0300

    Emacs: add initial config.

---
+++ home/paveloom/.config/emacs/init.el
@@ -0,0 +1,5 @@
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+(add-to-list 'default-frame-alist '(font . "JetBrainsMono Nerd Font-14"))
+
+(setopt server-name "server")
+(server-start)
