25e3e7666a424fd7eee8451fbbcd082b3b62080d
Pavel Sobolev <contact@paveloom.dev>
2025-10-14 22:21:00 +0300

    Emacs: save desktop sessions.

--- home/paveloom/.config/emacs/init.el
+++ home/paveloom/.config/emacs/init.el
@@ -1,5 +1,13 @@
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 (add-to-list 'default-frame-alist '(font . "JetBrainsMono Nerd Font-14"))
 
+;; The desktop.el package sets the `desktop-dirname' variable
+;; itself, but it picks the default value from the first element of
+;; the `desktop-path' list, which we make sure to prepare
+(setq desktop-dirname (file-name-concat user-emacs-directory "desktops"))
+(make-directory desktop-dirname t)
+(setopt desktop-path (list desktop-dirname))
+(desktop-save-mode 1)
+
 (setopt server-name "server")
 (server-start)
