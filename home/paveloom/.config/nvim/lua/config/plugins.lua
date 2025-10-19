b88a82a910908f096a77efafb20807092ea4f219
Pavel Sobolev <contact@paveloom.dev>
2025-10-19 17:37:37 +0300

    Neovim: extract the plugins logic into a separate file.

---
+++ home/paveloom/.config/nvim/lua/config/plugins.lua
@@ -0,0 +1,15 @@
+-- Bootstrap `mini.nvim`, use `mini.deps` for managing plugins afterwards
+
+local path_package = vim.fs.joinpath(vim.fn.stdpath("data"), "site")
+local path_mini = vim.fs.joinpath(path_package, "pack/deps/start/mini.nvim")
+if not vim.uv.fs_stat(path_mini) then
+  vim.notify("Installing `mini.nvim`...")
+  vim.system({
+    'git', 'clone', '--filter=blob:none',
+    'https://github.com/nvim-mini/mini.nvim', path_mini
+  }):wait()
+  vim.cmd('packadd mini.nvim | helptags ALL')
+  vim.notify("Installed `mini.nvim`")
+end
+
+require("mini.deps").setup({ path = { package = path_package } })
