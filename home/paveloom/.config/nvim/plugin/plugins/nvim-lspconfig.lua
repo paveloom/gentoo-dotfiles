098507bc0fee35b003c27c995a1f251ef82feb3e
Pavel Sobolev <contact@paveloom.dev>
2025-12-18 22:25:53 +0300

    Neovim: set up LSP support.

---
+++ home/paveloom/.config/nvim/plugin/plugins/nvim-lspconfig.lua
@@ -0,0 +1,30 @@
+MiniDeps.add({
+  source = "neovim/nvim-lspconfig",
+  checkout = "v2.5.0"
+})
+
+vim.lsp.enable("lua_ls")
+
+local group = vim.api.nvim_create_augroup("config.lsp", {})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = group,
+  callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    if client:supports_method("textDocument/completion") then
+      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+    end
+    if
+      not client:supports_method("textDocument/willSaveWaitUntil")
+      and client:supports_method("textDocument/formatting")
+    then
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = group,
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+        end,
+      })
+    end
+  end,
+})
