2b5a69b1ad95e4709827f7663c9dfd53fa15434a
Pavel Sobolev <contact@paveloom.dev>
2025-12-21 21:41:23 +0300

    Neovim: configure `nvim-tree` further.

--- home/paveloom/.config/nvim/plugin/plugins/nvim-tree.lua
+++ home/paveloom/.config/nvim/plugin/plugins/nvim-tree.lua
@@ -1,7 +1,50 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+require("nvim-tree").setup({
+  disable_netrw = true,
+  view = {
+    float = {
+      enable = true,
+      open_win_config = { row = 0, col = 0 }
+    },
+    width = { padding = 3 }
+  },
+  renderer = {
+    highlight_git = "name",
+    icons = {
+      diagnostics_placement = "after",
+      show = {
+        git = false
+      }
+    },
+    root_folder_label = function(path)
+      return vim.fn.pathshorten(vim.fn.fnamemodify(path, ":~"))
+    end,
+    special_files = {}
+  },
+  on_attach = function(buffer)
+    local api = require("nvim-tree.api")
 
-require("nvim-tree").setup()
+    local function opts(desc)
+      return {
+        desc = "nvim-tree: " .. desc,
+        buffer = buffer,
+        noremap = true,
+        silent = true,
+        nowait = true
+      }
+    end
+
+    api.config.mappings.default_on_attach(buffer)
+
+    vim.keymap.set("n", "<Esc>", api.tree.close, opts("Close"))
+  end,
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true
+  },
+  modified = {
+    enable = true
+  }
+})
 
 vim.keymap.set("n", "<leader>w", function()
   require("nvim-tree.api").tree.toggle({ find_file = true })
