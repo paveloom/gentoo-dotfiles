638bd3cb9ce22765833717df30213bb5e86cd76a
Pavel Sobolev <contact@paveloom.dev>
2025-12-21 13:58:31 +0300

    Neovim: use `<leader>w` to show the file tree.

--- home/paveloom/.config/nvim/plugin/plugins/nvim-tree.lua
+++ home/paveloom/.config/nvim/plugin/plugins/nvim-tree.lua
@@ -2,3 +2,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup()
+
+vim.keymap.set("n", "<leader>w", function()
+  require("nvim-tree.api").tree.toggle({ find_file = true })
+end)
