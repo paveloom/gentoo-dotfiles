88e107d78bf846ff28b5ea2464470bce447aeb65
Pavel Sobolev <contact@paveloom.dev>
2025-12-22 22:25:19 +0300

    Neovim: make code in `nvim-lspconfig.lua` more compact.

--- home/paveloom/.config/nvim/plugin/plugins/nvim-lspconfig.lua
+++ home/paveloom/.config/nvim/plugin/plugins/nvim-lspconfig.lua
@@ -7,7 +7,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
     if client:supports_method("textDocument/completion") then
-      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+      vim.lsp.completion.enable(
+        true, client.id, args.buf, { autotrigger = true }
+      )
     end
     if
       not client:supports_method("textDocument/willSaveWaitUntil")
@@ -17,9 +19,11 @@ vim.api.nvim_create_autocmd("LspAttach", {
         group = group,
         buffer = args.buf,
         callback = function()
-          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
-        end,
+          vim.lsp.buf.format({
+            bufnr = args.buf, id = client.id, timeout_ms = 1000
+          })
+        end
       })
     end
-  end,
+  end
 })
