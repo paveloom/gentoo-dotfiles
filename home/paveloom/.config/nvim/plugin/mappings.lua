f3df2e0ee085cef4aa4d579b477f63d25d1da16f
Pavel Sobolev <contact@paveloom.dev>
2025-12-23 21:51:37 +0300

    Neovim: jump by 5 lines when holding Ctrl and pressing Up/Down arrows.

--- home/paveloom/.config/nvim/plugin/mappings.lua
+++ home/paveloom/.config/nvim/plugin/mappings.lua
@@ -5,3 +5,9 @@ map("i", "<Down>", "<C-o>gj")
 map("i", "<Up>", "<C-o>gk")
 map({ "n", "v" }, "<Down>", "gj")
 map({ "n", "v" }, "<Up>", "gk")
+
+-- Jump by 5 lines with <C-Up> and <C-Down>
+map("i", "<C-Down>", "<C-o>5gj")
+map("i", "<C-Up>", "<C-o>5gk")
+map({ "n", "v" }, "<C-Down>", "5gj")
+map({ "n", "v" }, "<C-Up>", "5gk")
