94b7975233f1bd626ddce363125dec7abd044741
Pavel Sobolev <contact@paveloom.dev>
2026-01-02 17:49:42 +0300

    Neovim: update the theme to be Base24 compliant (sort of).

--- home/paveloom/.config/nvim/colors/custom.lua
+++ home/paveloom/.config/nvim/colors/custom.lua
@@ -1,114 +1,68 @@
--- To reload the theme, run `:colorscheme adwaita_light`
+-- To reload the theme, run `:colorscheme custom`
 
 if vim.g.colors_name ~= nil then vim.cmd.highlight("clear") end
-vim.g.colors_name = "custom"
+vim.g.colors_name    = "custom"
 
 ---@diagnostic disable: unused-local
 
--- See the GNOME HIG's palette reference at
--- https://developer.gnome.org/hig/reference/palette.html
+local shade_0        = "#000000"
+local shade_1        = "#B3B8B8"
+local shade_2        = "#ABB0B0"
+local shade_3        = "#B1B5B7"
+local shade_4        = "#BBBEBF"
+local shade_5        = "#C6C9CA"
+local shade_6        = "#E2E4E5"
+local shade_7        = "#ffffff"
 
-local blue_1 = "#99c1f1"
-local blue_2 = "#62a0ea"
-local blue_3 = "#3584e4"
-local blue_4 = "#1c71d8"
-local blue_5 = "#1a5fb4"
-local green_1 = "#8ff0a4"
-local green_2 = "#57e389"
-local green_3 = "#33d17a"
-local green_4 = "#2ec27e"
-local green_5 = "#26a269"
-local yellow_1 = "#f9f06b"
-local yellow_2 = "#f8e45c"
-local yellow_3 = "#f6d32d"
-local yellow_4 = "#f5c211"
-local yellow_5 = "#e5a50a"
-local orange_1 = "#ffbe6f"
-local orange_2 = "#ffa348"
-local orange_3 = "#ff7800"
-local orange_4 = "#e66100"
-local orange_5 = "#c64600"
-local red_1 = "#f66151"
-local red_2 = "#ed333b"
-local red_3 = "#e01b24"
-local red_4 = "#c01c28"
-local red_5 = "#a51d2d"
-local purple_1 = "#dc8add"
-local purple_2 = "#c061cb"
-local purple_3 = "#9141ac"
-local purple_4 = "#813d9c"
-local purple_5 = "#613583"
-local brown_1 = "#cdab8f"
-local brown_2 = "#b5835a"
-local brown_3 = "#986a44"
-local brown_4 = "#865e3c"
-local brown_5 = "#63452c"
-local light_1 = "#ffffff"
-local light_2 = "#f6f5f4"
-local light_3 = "#deddda"
-local light_4 = "#c0bfbc"
-local light_5 = "#9a9996"
-local dark_1 = "#77767b"
-local dark_2 = "#5e5c64"
-local dark_3 = "#3d3846"
-local dark_4 = "#241f31"
-local dark_5 = "#000000"
+local red            = "#a32a18"
+local brown          = "#6d4f00"
+local green          = "#247200"
+local emerald        = "#007f31"
+local teal           = "#006f81"
+local blue           = "#2c4ab3"
+local purple         = "#7527a8"
+local violet         = "#a71a68"
 
--- See the default Neovim theme reference at
--- https://github.com/nshern/neovim-default-colorscheme-extras
-
-local dark_blue = "#004c63"
-local dark_cyan = "#007373"
-local dark_green = "#005523"
-local dark_magenta = "#470045"
-local dark_red = "#590008"
-local dark_yellow = "#6b5300"
-local light_blue = "#A6DBFF"
-local light_cyan = "#8cf8f7"
-local light_green = "#b4f6c0"
-local light_magenta = "#FFCAFF"
-local light_red = "#FFC0B9"
-local light_yellow = "#FCE094"
-local light_grey_1 = "#EEF1F8"
-local light_grey_2 = "#E0E2EA"
-local light_grey_3 = "#C4C6CD"
-local light_grey_4 = "#9b9ea4"
-local dark_grey_4 = "#4f5258"
-local dark_grey_3 = "#2c2e33"
-local dark_grey_2 = "#14161B"
-local dark_grey_1 = "#07080D"
+local bright_red     = "#e75741"
+local bright_brown   = "#a68306"
+local bright_green   = "#4fad13"
+local bright_emerald = "#14bc5f"
+local bright_teal    = "#18a8be"
+local bright_blue    = "#597cf9"
+local bright_purple  = "#b052ec"
+local bright_violet  = "#eb43a0"
 
 local function hi(name, val)
   vim.api.nvim_set_hl(0, name, val)
 end
 
-hi("Normal", { bg = light_1, fg = dark_5 })
+hi("Normal", { bg = shade_7, fg = shade_0 })
 hi("NormalFloat", { link = "Normal" })
 
 hi("CursorLine", { link = "Visual" })
-hi("LineNR", { fg = light_5 })
-hi("StatusLine", { bg = light_2 })
-hi("StatusLineNC", { bg = light_3 })
-hi("Visual", { bg = light_3 })
+hi("LineNR", { fg = shade_2 })
+hi("StatusLine", { bg = shade_6 })
+hi("StatusLineNC", { bg = shade_5 })
+hi("Visual", { bg = shade_6 })
 
 hi("Pmenu", { link = "Normal" })
 hi("PmenuKind", { bg = "none" })
 hi("PmenuSel", { link = "Visual" })
-hi("PmenuThumb", { bg = light_5 })
+hi("PmenuThumb", { bg = shade_7 })
 
 -- Tree-sitter's capture groups are described at
 -- https://github.com/nvim-treesitter/nvim-treesitter/blob/master/CONTRIBUTING.md
 
-hi("@function", { fg = blue_5 })
-hi("@property", { fg = brown_5 })
-hi("@string", { fg = green_5 })
+hi("@function", { fg = blue })
+hi("@property", { fg = brown })
+hi("@string", { fg = green })
 
-hi("NvimTreeFolderIcon", { fg = dark_1 })
+hi("NvimTreeFolderIcon", { fg = shade_2 })
 
 hi("SnacksPickerBufFlags", { link = "SnacksPickerFile" })
 hi("SnacksPickerCol", { link = "SnacksPickerFile" })
 hi("SnacksPickerDir", { link = "SnacksPickerFile" })
 
-hi("GitSignsAdd", { fg = light_green })
-hi("GitSignsChange", { fg = light_yellow })
-hi("GitSignsDelete", { fg = light_red })
+hi("GitSignsAdd", { fg = bright_green })
+hi("GitSignsChange", { fg = bright_brown })
+hi("GitSignsDelete", { fg = bright_red })
