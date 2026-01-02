e846d94d693564083b276d6fd7f2a416833c7d65
Pavel Sobolev <contact@paveloom.dev>
2026-01-02 21:07:39 +0300

    Neovim: notify about messages from language servers.

--- home/paveloom/.config/nvim/plugin/plugins/nvim-lspconfig.lua
+++ home/paveloom/.config/nvim/plugin/plugins/nvim-lspconfig.lua
@@ -51,3 +51,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end)
   end
 })
+
+vim.api.nvim_create_autocmd("LspProgress", {
+  group = group,
+  callback = function()
+    vim.notify(vim.lsp.status())
+  end
+})
