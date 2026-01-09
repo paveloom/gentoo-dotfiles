608124e912793e356c8fa40ca280823c8041fe56
Pavel Sobolev <contact@paveloom.dev>
2026-01-09 17:43:38 +0300

    Neovim: add motions for `@function` and `@parameter` text objects.

--- home/paveloom/.config/nvim/init.lua
+++ home/paveloom/.config/nvim/init.lua
@@ -44,17 +44,18 @@ bootstrap({
 setup()
 
 add({
-  { source = "Darazaki/indent-o-matic",         checkout = "764da1f" },
-  { source = "folke/snacks.nvim",               checkout = "v2.30.0" },
-  { source = "j-hui/fidget.nvim",               checkout = "v1.6.1" },
-  { source = "lewis6991/gitsigns.nvim",         checkout = "v1.0.2" },
-  { source = "linrongbin16/gitlinker.nvim",     checkout = "v5.3.0" },
-  { source = "neovim/nvim-lspconfig",           checkout = "v2.5.0" },
-  { source = "nvim-mini/mini.hipatterns",       checkout = "v0.17.0" },
-  { source = "nvim-tree/nvim-tree.lua",         checkout = "v1.14.0" },
-  { source = "nvim-tree/nvim-web-devicons",     checkout = "v0.100" },
-  { source = "nvim-treesitter/nvim-treesitter", checkout = "v0.10.0" },
-  { source = "saghen/blink.cmp",                checkout = "v1.8.0" }
+  { source = "Darazaki/indent-o-matic",                     checkout = "764da1f" },
+  { source = "folke/snacks.nvim",                           checkout = "v2.30.0" },
+  { source = "j-hui/fidget.nvim",                           checkout = "v1.6.1" },
+  { source = "lewis6991/gitsigns.nvim",                     checkout = "v1.0.2" },
+  { source = "linrongbin16/gitlinker.nvim",                 checkout = "v5.3.0" },
+  { source = "neovim/nvim-lspconfig",                       checkout = "v2.5.0" },
+  { source = "nvim-mini/mini.hipatterns",                   checkout = "v0.17.0" },
+  { source = "nvim-tree/nvim-tree.lua",                     checkout = "v1.14.0" },
+  { source = "nvim-tree/nvim-web-devicons",                 checkout = "v0.100" },
+  { source = "nvim-treesitter/nvim-treesitter",             checkout = "v0.10.0" },
+  { source = "nvim-treesitter/nvim-treesitter-textobjects", checkout = "28a3494" },
+  { source = "saghen/blink.cmp",                            checkout = "v1.8.0" }
 })
 
 -- Allow project-local configuration
--- home/paveloom/.config/nvim/plugin/plugins/nvim-treesitter.lua
+++ home/paveloom/.config/nvim/plugin/plugins/nvim-treesitter.lua
@@ -8,3 +8,28 @@ require("nvim-treesitter.configs").setup({
     enable = true
   }
 })
+
+require("nvim-treesitter-textobjects").setup({
+  select = {
+    lookahead = true
+  }
+})
+
+local select = require("nvim-treesitter-textobjects.select")
+local function select_textobject(query_string)
+  select.select_textobject(query_string, "textobjects")
+end
+
+local mode = { "x", "o" }
+vim.keymap.set(mode, "af", function()
+  select_textobject("@function.outer")
+end)
+vim.keymap.set(mode, "if", function()
+  select_textobject("@function.inner")
+end)
+vim.keymap.set(mode, "aa", function()
+  select_textobject("@parameter.outer")
+end)
+vim.keymap.set(mode, "ia", function()
+  select_textobject("@parameter.inner")
+end)
