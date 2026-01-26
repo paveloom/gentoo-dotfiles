738cc4bc4e2bb16cf5a97bc377230400b5ac2a4a
Pavel Sobolev <contact@paveloom.dev>
2026-01-26 21:35:30 +0300

    Neovim: set up the `kylechui/nvim-surround` plugin.

--- home/paveloom/.config/nvim/init.lua
+++ home/paveloom/.config/nvim/init.lua
@@ -47,6 +47,7 @@ add({
   { source = "Darazaki/indent-o-matic",                     checkout = "764da1f" },
   { source = "folke/snacks.nvim",                           checkout = "v2.30.0" },
   { source = "j-hui/fidget.nvim",                           checkout = "v1.6.1" },
+  { source = "kylechui/nvim-surround",                      checkout = "v3.1.8" },
   { source = "lewis6991/gitsigns.nvim",                     checkout = "v1.0.2" },
   { source = "linrongbin16/gitlinker.nvim",                 checkout = "v5.3.0" },
   { source = "neovim/nvim-lspconfig",                       checkout = "v2.5.0" },
