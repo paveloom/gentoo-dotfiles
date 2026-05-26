6711cb32c3ec487dace6a2ad573bf4b6b281d976
Pavel Sobolev <contact@paveloom.dev>
2026-05-26 22:17:18 +0300

    Neovim: switch to the built-in package manager.

--- home/paveloom/.config/nvim/init.lua
+++ home/paveloom/.config/nvim/init.lua
@@ -1,62 +1,56 @@
-local site = vim.fs.joinpath(vim.fn.stdpath("data"), "site")
-
-local function bootstrap(spec)
-  local name = spec.name or vim.fn.fnamemodify(spec.source, ":t")
-  local path = vim.fs.joinpath(site, "pack/deps/start", name)
-  if not vim.uv.fs_stat(path) then
-    vim.notify("Bootstrapping `" .. name .. "`...")
-    local url = "https://github.com/" .. spec.source
-    vim.system({
-      "git", "clone", "--filter=blob:none",
-      "--branch", spec.checkout,
-      url, path
-    }):wait()
-    vim.cmd.packadd(name)
-    vim.cmd.helptags(vim.fs.joinpath(path, "doc"))
-  end
-end
-
-local function setup()
-  require("mini.deps").setup({
-    job = { timeout = 60000 },
-    path = { package = site },
-    silent = true
-  })
-end
-
----@param specs table[]
-local function add(specs)
-  for _, spec in ipairs(specs) do
-    local name = spec.name or vim.fn.fnamemodify(spec.source, ":t")
-    local path = vim.fs.joinpath(site, "pack/deps/opt", name)
-    if not vim.uv.fs_stat(path) then
-      vim.notify("Installing `" .. name .. "`...")
-    end
-    require("mini.deps").add(spec, {})
-  end
-end
-
-bootstrap({
-  source = "nvim-mini/mini.deps",
-  checkout = "v0.17.0"
-})
-
-setup()
-
-add({
-  { source = "Darazaki/indent-o-matic",                     checkout = "764da1f" },
-  { source = "folke/snacks.nvim",                           checkout = "v2.31.0" },
-  { source = "j-hui/fidget.nvim",                           checkout = "v1.6.1" },
-  { source = "kylechui/nvim-surround",                      checkout = "v4.0.5" },
-  { source = "lewis6991/gitsigns.nvim",                     checkout = "v2.1.0" },
-  { source = "linrongbin16/gitlinker.nvim",                 checkout = "v5.4.0" },
-  { source = "neovim/nvim-lspconfig",                       checkout = "v2.9.0" },
-  { source = "nvim-mini/mini.hipatterns",                   checkout = "v0.17.0" },
-  { source = "nvim-tree/nvim-tree.lua",                     checkout = "v1.17.0" },
-  { source = "nvim-tree/nvim-web-devicons",                 checkout = "v0.100" },
-  { source = "nvim-treesitter/nvim-treesitter",             checkout = "4916d65" },
-  { source = "nvim-treesitter/nvim-treesitter-textobjects", checkout = "851e865" },
-  { source = "saghen/blink.cmp",                            checkout = "v1.10.2" }
+vim.pack.add({
+  {
+    src = "https://github.com/Darazaki/indent-o-matic",
+    version = "764da1f"
+  },
+  {
+    src = "https://github.com/folke/snacks.nvim",
+    version = vim.version.range("^2")
+  },
+  {
+    src = "https://github.com/j-hui/fidget.nvim",
+    version = vim.version.range("^1")
+  },
+  {
+    src = "https://github.com/kylechui/nvim-surround",
+    version = vim.version.range("^4")
+  },
+  {
+    src = "https://github.com/lewis6991/gitsigns.nvim",
+    version = vim.version.range("^2")
+  },
+  {
+    src = "https://github.com/linrongbin16/gitlinker.nvim",
+    version = vim.version.range("^5")
+  },
+  {
+    src = "https://github.com/neovim/nvim-lspconfig",
+    version = vim.version.range("^2")
+  },
+  {
+    src = "https://github.com/nvim-mini/mini.hipatterns",
+    version = vim.version.range("^0.17")
+  },
+  {
+    src = "https://github.com/nvim-tree/nvim-tree.lua",
+    version = vim.version.range("^1")
+  },
+  {
+    src = "https://github.com/nvim-tree/nvim-web-devicons",
+    version = vim.version.range("v0.100")
+  },
+  {
+    src = "https://github.com/nvim-treesitter/nvim-treesitter",
+    version = "4916d65"
+  },
+  {
+    src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
+    version = "851e865"
+  },
+  {
+    src = "https://github.com/saghen/blink.cmp",
+    version = vim.version.range("^1")
+  }
 })
 
 -- Allow project-local configuration
