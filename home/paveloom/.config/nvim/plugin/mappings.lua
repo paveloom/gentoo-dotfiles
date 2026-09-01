81802c459bfc34b184a507670a958bcb66b32a6f
Pavel Sobolev <contact@paveloom.dev>
2026-09-01 18:52:47 +0300

    Neovim: break the undo block after every space

--- home/paveloom/.config/nvim/plugin/mappings.lua
+++ home/paveloom/.config/nvim/plugin/mappings.lua
@@ -53,6 +53,9 @@ map("x", "p", "P")
 -- Open a vertical split instead of a horizontal one with `<C-w><C-]>`
 map("n", "<C-w><C-]>", "<cmd>vert wincmd ]<cr>")
 
+-- Break the undo block after every space
+map("i", "<space>", "<C-g>u<space>")
+
 -- Unbind <C-LeftMouse> which does the same as `<C-]` by default
 map("n", "<C-LeftMouse>", "<nop>")
 
